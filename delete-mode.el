970095e93a33ddcffcc0368220f4d17ffdeb5733
Adrian Teng <adrian@teng.io>
2015-01-12 00:37:00 +0000

    Add subword-mode support (incomplete with key binding)

--- delete-mode.el
+++ delete-mode.el
@@ -12,21 +12,38 @@
 
 (defun delete-word (arg)
   "Delete characters forward until encountering the end of a word.
-With ARG, do this that many times."
+With argument ARG, do this that many times."
   (interactive "p")
   (delete-region (point) (progn (forward-word arg) (point))))
 
 (defun backward-delete-word (arg)
   "Delete characters backward until encountering the end of a word.
-With ARG, do this that many times."
+With argument ARG, do this that many times."
   (interactive "p")
   (delete-word (- arg)))
+
+
+(defun subword-delete (arg)
+  "Delete characters forward until encountering the end of a subword.
+With argument ARG, do this that many times."
+  (interactive "p")
+  (delete-region (point) (subword-forward arg)))
+
+(defun backward-subword-delete (arg)
+  "Delete characters backward until encountering the end of a subword.
+With argument ARG, do this that many times."
+  (interactive "p")
+  (subword-delete (- arg)))
 
 (defvar delete-mode-map (make-keymap)
   "Kepmap for delete-mode.")
 
 (define-key delete-mode-map [remap kill-word] 'delete-word)
 (define-key delete-mode-map [remap backward-kill-word] 'backward-delete-word)
+(define-key delete-mode-map [remap subword-kill] 'subword-delete)
+(define-key delete-mode-map [remap subword-backward-kill] 'backward-subword-delete)
+
+
 
 (define-minor-mode delete-mode
  "This package replaces a collection of kill commands shortcuts with a respective delete version.
